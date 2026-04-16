700e6b3dc777cda43f9600305104540d9d0f1d40
Bernstain Fangon <fangonbernstain566@gmail.com>
2026-04-16 22:24:13 +0800

    Update main.js with new game configuration

--- src/game/main.js
+++ src/game/main.js
@@ -1,18 +1,25 @@
+import Phaser, { AUTO, Game } from 'phaser';
 import { Boot } from './scenes/Boot';
 import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
-import { AUTO, Game } from 'phaser';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 960,
+    height: 540,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#1b1b2f',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
@@ -27,7 +34,7 @@ const config = {
 };
 
 const StartGame = (parent) => {
-
+    
     return new Game({ ...config, parent });
 
 }
